0cd35403d9757bdd2c91a971b2cadd2d8925a5e0
@dev.arthur <contato.arthurreis222@gmail.com>
2024-10-09 09:54:08 -0300

    chore: add isAuthenticated middleware

---
+++ src/shared/middlewares/isAuthenticated.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, RequestHandler, Response } from "express";
+
+import { verify } from "jsonwebtoken";
+import { UnauthorizedError } from "../helpers/errors";
+
+interface Payload {
+  sub: string;
+}
+
+export const isAuthenticated = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authToken = req.headers.authorization;
+
+  if (!authToken) throw new UnauthorizedError("O usuário não está autenticado");
+
+  const [, token] = authToken.split(" ");
+
+  try {
+    const { sub } = verify(token, process.env.JWT_SECRET as string) as Payload;
+    req.userId = sub;
+    return next();
+  } catch (error) {
+    throw new UnauthorizedError("O usuário não está autenticado");
+  }
+};
