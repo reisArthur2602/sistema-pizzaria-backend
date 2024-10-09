cdb007f3fb44630414738e1466596e0c353f3958
@dev.arthur <contato.arthurreis222@gmail.com>
2024-10-09 07:17:06 -0300

    chore: setup initial

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import "dotenv/config"
+
+import express from "express";
+import cors from "cors";
+
+import { router } from "./routes";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(router);
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
